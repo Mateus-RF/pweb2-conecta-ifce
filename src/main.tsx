40fdc19efdd01dfe5fc598917548ec1d8f806f69
Mateus Ribeiro Ferreira <90785350+Mateus-RF@users.noreply.github.com>
2026-03-20 15:38:12 -0300

    feat: reorganiza estrutura de rotas e layouts, adiciona AppLayout e PublicLayout

--- src/main.tsx
+++ src/main.tsx
@@ -1,44 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { createBrowserRouter } from 'react-router'
-import LoginPage from './pages/LoginPage.tsx'
-import HomePage from './pages/homepage/HomePage.tsx'
-import RegisterPage from './pages/RegisterPage.tsx'
-import { RouterProvider } from 'react-router/dom'
-import { AuthProvider } from './features/auth/contexts/AuthContext.tsx'
-import FeedPage from './pages/FeedPage.tsx'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: App,
-    children: [
-      {
-        index: true,
-        Component: HomePage,
-      },
-      {
-        path: 'login',
-        Component: LoginPage,
-      },
-      {
-        path: 'register',
-        Component: RegisterPage,
-      },
-      {
-        path:'feed',
-        Component: FeedPage,
-      }
-    ],
-  },
-])
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <App/>
   </StrictMode>,
 )
